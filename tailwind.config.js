2cd25c1f2933479fb4acace772e63209320f1c02
mizgaionutalexandru <mizgaionutalexandru@gmail.com>
2022-08-02 14:40:56 +0300

    added header and title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    colors: {
+      "yellow-1": "#ffe600",
+      "yellow-2": "#ffe605",
+      neutral: colors.neutral,
+    },
     extend: {},
   },
   plugins: [],
